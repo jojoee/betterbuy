73c00938448159b0191300869e70e8ce517e1745
Nathachai Thongniran <inid3a@gmail.com>
2026-08-14 17:17:25 +0700

    feat(icon): consolidate Betterbuy app icon

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'betterbuy-v1';
-const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icons/betterbuy-overlap-a.svg'];
+const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icons/betterbuy-overlap.svg'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL))));
 self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
